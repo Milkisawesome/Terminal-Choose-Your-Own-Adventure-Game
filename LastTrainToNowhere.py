64b44d8b5bdd6ecee759e8377e02bf96c87c12d9
Milkisawesome <muzzammil.sheik1@gmail.com>
2026-08-06 20:36:24 +0200

    Added game menu. Typing speed produces error and prints line by line

--- LastTrainToNowhere.py
+++ LastTrainToNowhere.py
@@ -11,12 +11,30 @@ def text_speed(text,delay=0.03):
         print()
 
 ##Gets choices and validates the users input
-    def get_choice(option):
-        while True
-            choice = input("/What do you want to do").strip().lower()
-            if choice in option:
+def get_choice(options):
+        while True:
+            choice = input("\nWhat do you want to do").strip().upper()
+            if choice in options:
                 return choice
             print("Invalid choice. Please try again.")
 
 
-def game_start)():
+def game_menu():
+    text_speed("---- THE LAST TRAIN TO NOWHERE ----")
+    text_speed("[A] Start Game")
+    text_speed("[B] Quit")
+
+    choice = get_choice(['A', 'B'])
+    if choice == 'A':
+        start_game()
+    else:
+        text_speed("Quitting... Thank you for playing!")
+        sys.exit()
+
+
+def start_game():
+    print("Welcome to Last Train To Nowhere")
+
+
+if __name__ == "__main__":
+    game_menu()
